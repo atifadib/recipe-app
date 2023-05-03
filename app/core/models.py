006da6beee6dee7f8e322322ee3ca63e56b377b2
atifadib <atif.adib25@gmail.com>
2023-05-03 16:27:02 +0530

    adding changes for User Model and its tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,2 +1,31 @@
-# from django.db import models
-# Create your models here.
+"""
+database models
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for user model"""
+    def create_user(self, email, password=None, **extrafields):
+        """create/save/return new users"""
+        if not email:
+            raise ValueError('User must have a valid Email Address')
+        user = self.model(email=self.normalize_email(email), **extrafields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,37 @@
+"""
+Test for Models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models"""
+
+    def test_create_user_with_email_success(self):
+        """test creating user"""
+        email = 'test@example.com'
+        passwd = 'testpass@123'
+        user = get_user_model().objects.create_user(
+            email=email, password=passwd
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(passwd))
+
+    def test_new_user_email_normalize(self):
+        """Normalize email addresses"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for real, expected in sample_emails:
+            user = get_user_model().objects.create_user(real, 'pass@123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_email_valid(self):
+        """Check if user has a valid email address"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'oass@123')
